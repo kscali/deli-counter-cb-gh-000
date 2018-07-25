8af4def7d91760a7574eeb98046002df38d7fee7
Koy Saeteurn <kosatun@aol.com>
2018-07-25 23:49:55 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,8 +7,13 @@ def line
 end
 
 def take_a_number(katz_deli, name)
+  katz_deli.push(name)
+  new_line = ""
   position = 0 
-  puts "
-  
+  katz_deli.each do |person|
+    new_line += position + ". " + person
+  position += 1  
+  end  
+  puts "The line is currently: " + new_line
 end  
   
