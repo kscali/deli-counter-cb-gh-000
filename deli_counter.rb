a14ab1a834fb968bef337136259ebb5a5410edb1
Koy Saeteurn <kosatun@aol.com>
2018-07-25 23:56:50 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -9,15 +9,17 @@ end
 def take_a_number(katz_deli, name)
   katz_deli.push(name)
   new_line = ""
-  position = 0 
+  position = 1 
   katz_deli.each do |person|
-    new_line += "#{position}. #{person}"
+    new_line += "#{position}. #{person} "
   position += 1  
   end  
   puts "The line is currently: #{new_line}"
 end  
 
 def now_serving(katz_deli)
+  if katz_deli == []
+    puts "There is nobody waiting to be served!"
   
 end  
   
